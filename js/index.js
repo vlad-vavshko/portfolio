ad2aafcbd92176fb19369712d692f958b571ccb4
vlad-vavshko <68970447+vlad-vavshko@users.noreply.github.com>
2021-10-13 20:51:56 +0300

    add light box and styles

--- js/index.js
+++ js/index.js
@@ -3,12 +3,25 @@
         var file = document.getElementById( 'folder-inside' );
         var closeBtn = document.getElementById( 'close' );
         var modal = document.getElementsByClassName( 'modal-container' )[0];
+        var pos = document.documentElement;
+        var lightBox = document.getElementsByClassName( 'light' )[0];
 
+        //open modal
         file.addEventListener( "click", function () {
             modal.style.display = 'flex'
+            lightBox.style.display = 'none'
         } );
         //close modal
         closeBtn.addEventListener( "click", function () {
             modal.style.display = 'none'
+            lightBox.style.display = 'block'
+
         } );
+
+        // Light
+        pos.addEventListener( 'mousemove', e => {
+            pos.style.setProperty( '--x', e.clientX + 'px' )
+            pos.style.setProperty( '--y', e.clientY + 'px')
+            
+        })
     });
