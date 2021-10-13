4f667ffc26b7a37dcad30e16a41e716c8875fe94
vlad-vavshko <68970447+vlad-vavshko@users.noreply.github.com>
2021-10-13 16:22:10 +0300

    adding popup and cursor for file

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
     document.addEventListener('DOMContentLoaded', function() {
-        // Glitch Logo
-        
+        // open modal
+        var file = document.getElementById( 'folder-inside' );
+        var closeBtn = document.getElementById( 'close' );
+        var modal = document.getElementsByClassName( 'modal-container' )[0];
 
-
-        
+        file.addEventListener( "click", function () {
+            modal.style.display = 'flex'
+        } );
+        //close modal
+        closeBtn.addEventListener( "click", function () {
+            modal.style.display = 'none'
+        } );
     });
