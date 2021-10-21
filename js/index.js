54a9c6def23d79b74a5899ea2c1f7036de2139db
vlad-vavshko <68970447+vlad-vavshko@users.noreply.github.com>
2021-10-21 21:30:23 +0300

    adding adaptive styles

--- js/index.js
+++ js/index.js
@@ -56,6 +56,21 @@
             console.log(0)
         } );
 
-
-
+        //add scroll clas to modal-content
+        
+        var modalContent = document.querySelector('.modal-content');
+        if (window.innerWidth < 1561) {
+            modalContent.classList.add('scroll-container');
+            console.log(true)
+        } else {
+            modalContent.classList.remove('scroll-container');
+        }
+        window.addEventListener("resize", function() {
+            if (window.innerWidth < 1561) {
+                modalContent.classList.add('scroll-container');
+                console.log(true)
+            } else {
+                modalContent.classList.remove('scroll-container');
+            }
+        }, true);
     });
