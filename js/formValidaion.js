bfcafcc1d1185905841ae677779d4a5f11629ef3
vladyslav-vavshko <68970447+vlad-vavshko@users.noreply.github.com>
2021-11-01 17:18:51 +0200

    add inputs to the form

--- js/formValidaion.js
+++ js/formValidaion.js
@@ -1,65 +1,43 @@
 $(document).ready(() => {
-        let firstName = $('input[name=first-name]');
-        let lastName = $('input[name=last-name]');
-        let email = $('input[name=email]');
-        let emailMessage = $('textarea[name=message]');
+        
+  function validateForm() {
+    let firstName = document.querySelector('input[name=first-name]');
+    let lastName = document.querySelector('input[name=last-name]');
+    let email = document.querySelector('input[name=email]');
+    let emailMessage = document.querySelector('textarea[name=message]');
 
-        $('#formSubmit').click(() => {
+    $('#formSubmit').click(() => {
 
-            if(firstName.val() && lastName.val() && email.val() && emailMessage.val()) {
-                $.ajax({
-                    type: "post",
-                    url: "send-email.php",
-                    data:
-                      "first_name=" +
-                      firstName.val() +
-                      "&lastName=" +
-                      lastName.val() +
-                      "&email=" +
-                      email.val() +
-                      "&message=" +
-                      emailMessage.val(),
-                    success: () => {
-                      $('.form-modal-content .content-wrapper').hide();
-                      $('.form-modal-title').html('Thanks for contacting me! <br> I will be in touch with you shortly!')
-                    },
-                    error: () => {
-                      alert(
-                        "Email service is not working."
-                      );
-                    },
-                  });
-
+        if(!firstName.value && !lastName.value && !email.value && !emailMessage.value) {
+            if(!firstName.val()) {
+                firstName.classList.add('error-true');
+                firstName.nextSibling.nextSibling.classList.add('error-true');
             } else {
-                if(!firstName.val()) {
-                    firstName.addClass('error-true');
-                    firstName.next().next().addClass('error-true');
-                } else {
-                    firstName.removeClass('error-true');
-                    firstName.next().next().removeClass('error-true');
-                }
-                if(!lastName.val()) {
-                    lastName.addClass('error-true');
-                    lastName.next().next().addClass('error-true');
-                } else {
-                    lastName.removeClass('error-true');
-                    lastName.next().next().removeClass('error-true');
-                }
-                if(!email.val()) {
-                    email.addClass('error-true');
-                    email.next().next().addClass('error-true');
-                } else {
-                    email.removeClass('error-true');
-                    email.next().next().removeClass('error-true');
-                }
-                if(!emailMessage.val()) {
-                    emailMessage.addClass('error-true');
-                    emailMessage.next().next().addClass('error-true');
-                } else {
-                    emailMessage.removeClass('error-true');
-                    emailMessage.next().next().removeClass('error-true');
-                }
-                
+                firstName.classList.remove('error-true');
+                firstName.nextSibling.nextSibling.classList.remove('error-true');
             }
-        })
+            if(!lastName.val()) {
+                lastName.classList.add('error-true');
+                lastName.nextSibling.nextSibling.classList.add('error-true');
+            } else {
+                lastName.classList.remove('error-true');
+                lastName.nextSibling.nextSibling.classList.remove('error-true');
+            }
+            if(!email.val()) {
+                email.classList.add('error-true');
+                email.nextSibling.nextSibling.classList.add('error-true');
+            } else {
+                email.classList.remove('error-true');
+                email.nextSibling.nextSibling.classList.remove('error-true');
+            }
+            if(!emailMessage.val()) {
+                emailMessage.classList.add('error-true');
+                emailMessage.nextSibling.nextSibling.classList.add('error-true');
+            } else {
+                emailMessage.classList.remove('error-true');
+                emailMessage.nextSibling.nextSibling.classList.remove('error-true');
+            }            
+        }
+    })
+    }
 });
