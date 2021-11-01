c89018f9afd4550ebb3d89aa82dd92ea29ea0be6
vladyslav-vavshko <68970447+vlad-vavshko@users.noreply.github.com>
2021-11-01 17:19:43 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -77,7 +77,7 @@
         });
 
 
-        window.setTimeout(stayedLongUser,3000)
+        window.setTimeout(stayedLongUser,60000)
 
         function stayedLongUser() {
             modalFormPopup.classList.add( 'open' )
